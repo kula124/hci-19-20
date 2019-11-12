347c26772c1585f84b841aeca741490ea6caef29
kula <kula124@gmail.com>
2019-11-12 19:39:02 +0000

    home page init

---
+++ src/modules/PageHeading/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import  './style.module.scss'
+
+const PageHeading = () => 
+  <div styleName={style['container']}>
+    <h1 styleName='heading-top welcome-paragraph'>Welcome to </h1>
+    <h1>CodeJar</h1>
+    <h1 styleName='heading-top sdf-paragraph'>Software development firm</h1>
+    <h1 styleName='heading-top peak'> Take a peak</h1>  
+  </div>
+
+export default PageHeading
