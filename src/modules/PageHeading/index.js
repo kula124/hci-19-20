fec4d2be5aa1cd2807dcde38a8f3e94ff9ef9dea
kula <kula124@gmail.com>
2019-11-14 10:40:09 +0100

    Adding flat button to home page

--- src/modules/PageHeading/index.js
+++ src/modules/PageHeading/index.js
@@ -1,6 +1,7 @@
 import React from 'react'
 // import Img from 'gatsby-image'
 import Image from '../../components/image'
+import FlatButton from '../../components/FlatButton'
 
 import './style.module.scss'
 
@@ -9,7 +10,9 @@ const PageHeading = () =>
     <h1>Welcome to </h1>
     <Image />
     <h1>Software development firm</h1>
-    <h1> Take a peek</h1>  
+    <FlatButton>
+      <span>Take a peak</span>
+    </FlatButton>
   </div>
 
 export default PageHeading
