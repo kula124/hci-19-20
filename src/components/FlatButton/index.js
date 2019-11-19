c6477c6104fdd9c399cd03b30a1d19d30c280746
kula <kula124@gmail.com>
2019-11-20 00:19:36 +0100

    adding propTypes

--- src/components/FlatButton/index.js
+++ src/components/FlatButton/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
 import './style.module.scss'
 
-const FlatButton = ({onClick, styleProp, children}) => 
-    <button onClick={onClick} styleName={styleProp || 'default'}>
-        {children}
-    </button>
+const FlatButton = ({ onClick, styleProp, children }) =>
+  <button onClick={onClick}
+    styleName={styleProp || 'default'}>
+    {children}
+  </button>
+
+FlatButton.propTypes = {
+  children: PropTypes.array,
+  onClick: PropTypes.func.isRequired,
+  styleProp: PropTypes.string
+}
 
 export default FlatButton
