c0fd2b714bc79099b9e12b4b57383ea6d783337c
Ivan <kula124@gmail.com>
2019-12-07 17:05:59 +0100

    Feature/numbers module (#8)
    * removing body margin
    
    * formating scss a bit
    
    * adding svg-loader plugin and count up component
    
    * restructure gobal css
    
    * Adding initial CountingCard
    
    * adding required images
    
    * adding inital Numbers module
    
    * font size ++
    
    * Adding NumberSection
    
    * add real numbers and messages
    
    * adding loadash.get
    
    * adding image query and mapping
    
    * adding and renaming images
    
    * positioning elements

---
+++ src/components/CountingCard/index.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import Img from 'gatsby-image'
+
+import Count from 'react-countup'
+
+import './style.module.scss'
+
+const CountingCard = ({ image, text, count }) => {
+  return (<li>
+    <figure styleName='main-container'>
+      <div styleName='image-container'>
+        <Img fixed={image.fixed} />
+      </div>
+      <Count
+        duration={4}
+        end={count}
+        start={0} />
+      <figcaption>
+        {text}
+      </figcaption>
+    </figure>
+  </li>)
+}
+
+CountingCard.propTypes = {
+  count: PropTypes.number,
+  image: PropTypes.func.isRequired,
+  text: PropTypes.string
+
+}
+
+export default CountingCard
