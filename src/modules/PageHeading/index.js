9b5cea9fdbbb6c6a84455b5b1d82cb428d10312c
kula <kula124@gmail.com>
2019-12-07 17:01:44 +0100

    extracting logo image

--- src/modules/PageHeading/index.js
+++ src/modules/PageHeading/index.js
@@ -3,43 +3,28 @@ import MaterialIcon from '@material/react-material-icon'
 
 import LazyTypewriter from 'components/LazyTypewriter'
 import FlatButton from 'components/FlatButton'
-import Img from 'gatsby-image'
-import { useStaticQuery, graphql } from 'gatsby'
+import Logo from 'components/Images/Logo'
 
 import './style.module.scss'
 
-const PageHeading = () => {
-  // Loading image
-  const imgData = useStaticQuery(graphql`
-    query {
-      codejarLogo: file(relativePath: { eq: "logo.png" }) {
-        childImageSharp {
-          fixed(width: 60, height: 60, quality:100) {
-            ...GatsbyImageSharpFixed_tracedSVG
-          }
-        }
-      }
-    }
-  `)
-
-  return (
-    <div styleName='main-container'>
-      <h1>Welcome to </h1>
-      <figure>
-        <Img fixed={imgData.codejarLogo.childImageSharp.fixed} />
-        <figcaption>
-          <span styleName='code'>&lt;</span>
-          <LazyTypewriter string={'Codejar '}
-            tries={3} />
-          <span styleName='code'>/&gt;</span>
-        </figcaption>
-      </figure>
-      <h1>Software development firm</h1>
-      <FlatButton>
-        <span>Take a peak</span>
-        <MaterialIcon icon='arrow_forward' />
-      </FlatButton>
-    </div>)
-}
+const PageHeading = () => (
+  <div styleName='main-container'>
+    <h1>Welcome to </h1>
+    <figure>
+      <Logo />
+      <figcaption>
+        <span styleName='code'>&lt;</span>
+        <LazyTypewriter string={'Codejar '}
+          tries={3} />
+        <span styleName='code'>/&gt;</span>
+      </figcaption>
+    </figure>
+    <h1>Software development firm</h1>
+    <FlatButton>
+      <span>Take a peak</span>
+      <MaterialIcon icon='arrow_forward' />
+    </FlatButton>
+  </div>
+)
 
 export default PageHeading
