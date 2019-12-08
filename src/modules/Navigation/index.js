2edbf2f1018638c64412f82ae1436d8cae2d9406
kula <kula124@gmail.com>
2019-12-08 17:43:33 +0100

    making Navigation bar animated

--- src/modules/Navigation/index.js
+++ src/modules/Navigation/index.js
@@ -1,32 +1,53 @@
-import React from 'react'
+import React, { useState } from 'react'
 
+import { links } from './navigationData'
 import Logo from 'components/Images/Logo'
 import Typewriter from 'typewriter-effect'
+import PropTypes from 'prop-types'
 
 import './style.module.scss'
 
-const Navigation = () => {
+const Navigation = ({ refs }) => {
+  const [sticky, setSticky] = useState(false)
+  const [visible, setVisible] = useState(false)
+
+  window.onscroll = () => {
+    if (document.documentElement.scrollTop < window.innerHeight + 100) {
+      setSticky(false)
+    } else {
+      setSticky(true)
+    }
+
+    if (document.documentElement.scrollHeight >= window.innerHeight) {
+      setVisible(true)
+    }
+  }
+
   return (
-    <div styleName='main-container'>
+    <div className={sticky ? 'animated slideInDown' : 'animated slideInUp'}
+      styleName={sticky ? 'main-container sticky' : 'main-container' }>
       <div styleName='logo'>
         <Logo />
         <p>&lt;</p>
-        <Typewriter options={{
-          autoStart: true,
-          strings: 'CodeJar'
-        }} />
+        {visible && <Typewriter
+          options={{
+            autoStart: true,
+            delay: 400,
+            strings: 'CodeJar'
+          }} />}
         <p>/&gt;</p>
       </div>
       <nav>
-        <li>Technology</li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>Blog</li>
-        <li>Jobs</li>
-        <li>Login</li>
+        {links.map(el => <li key={el.id}>
+          {el.label}
+        </li>)}
       </nav>
     </div>
   )
+}
+
+Navigation.propTypes = {
+  refs: PropTypes.object
 }
 
 export default Navigation
