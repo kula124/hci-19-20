2ad0ab794bd4cbf639e8341c2a3fd846778021cd
kula <kula124@gmail.com>
2019-12-08 18:16:34 +0100

    adding scroll functions

--- src/helper.js
+++ src/helper.js
@@ -1,3 +1,5 @@
 export const GenerateRandomString = (len) => len > 4
   ? GenerateRandomString(len - 4).concat(Math.random().toString(36).substring(1, 5))
   : Math.random().toString(36).substring(1, len) // can generate upto 5 charrs
+
+export const ScrollToRef = ref => window.scrollTo(0, ref.current.offsetTop)
