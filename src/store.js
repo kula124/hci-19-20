fe0aa1862131cadd0a30677c095ac99b99060a5a
kula <kula124@gmail.com>
2019-12-14 23:59:33 +0100

    created store

---
+++ src/store.js
@@ -0,0 +1,58 @@
+import React, { useContext, useReducer, createContext } from 'react'
+import { AUTH, NAVIGATION } from 'constants/actions'
+const initialStore = {
+  auth: {
+    inProgress: false,
+    token: null,
+    user: {}
+  },
+  error: null,
+  navigation: []
+}
+
+const StoreContext = createContext(initialStore)
+
+const mainReducer = (state, action) => {
+  switch (action.type) {
+    case AUTH.IN_PROGRESS:
+      return ({
+        auth: {
+          ...state.auth,
+          inProgress: true
+        }
+      })
+    case AUTH.SUCCESS:
+      return ({
+        ...state,
+        auth: action.data
+      })
+    case AUTH.FAILED:
+      return ({
+        ...state,
+        error: action.error
+      })
+    case NAVIGATION.ADD_VISIBLE:
+      return ({
+        ...state,
+        navigation: [...state.navigation, action.data]
+      })
+    case NAVIGATION.REMOVE_VISIBLE:
+      return ({
+        ...state,
+        navigation: [...(state.navigation.filter(e => e !== action.data))]
+      })
+    default:
+      return state
+  }
+}
+
+export const StoreProvider = (props) => {
+  const [store, dispatch] = useReducer(mainReducer, initialStore)
+
+  const storeData = { store, dispatch }
+
+  return <StoreContext.Provider value={storeData}
+    {...props} />
+}
+
+export const useStore = () => useContext(StoreContext)
