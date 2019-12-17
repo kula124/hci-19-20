274436238f332456e48cdcfa90a9dc5e6cbec253
Ivan <kula124@gmail.com>
2019-12-17 20:48:40 +0100

    Global store added (#12)
    * created store
    
    * created visibility hook
    
    * creating actions
    
    * creating action creators for visible
    
    * typos
    
    * connecting navigation to the store
    
    * connecting tech to the store
    
    * adding selected style
    
    * connecting the store to root of the app

--- src/modules/Numbers/index.js
+++ src/modules/Numbers/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { useStaticQuery, graphql } from 'gatsby'
 import get from 'lodash.get'
 import keyBy from 'lodash.keyby'
+import VisibilitySensor from 'hooks/useIsVisible'
 
 import List from 'components/DelayedList'
 import data from './data'
@@ -35,22 +36,26 @@ const Numbers = React.forwardRef((props, ref) => {
       `).allFile.edges,
     e => get(e, 'node.childImageSharp.fixed.originalName')) // eslint-disable-line
 
-  return (<section ref={ref}
-    styleName='main-container'>
-    <h1>
-      May the numbers be ever in your favour!
-    </h1>
-    <p>
-      Luckily, they are in ours. Let the numbers speak! Experience in number of
-      challenging projects, sleek and clean code and happiness of our clients gives us confidence
-      in tackling any challenge you may throw at us!
-    </p>
-    <List>
-      {data.map(({ image, ...rest }, index) => <CountingCard image={images[image]}
-        key={index}
-        {...rest} />)}
-    </List>
-  </section>)
+  return (
+    <VisibilitySensor id='tech'>
+      <section ref={ref}
+        styleName='main-container'>
+        <h1>
+          May the numbers be ever in your favour!
+        </h1>
+        <p>
+          Luckily, they are in ours. Let the numbers speak! Experience in number of
+          challenging projects, sleek and clean code and happiness of our clients gives us confidence
+          in tackling any challenge you may throw at us!
+        </p>
+        <List>
+          {data.map(({ image, ...rest }, index) => <CountingCard image={images[image]}
+            key={index}
+            {...rest} />)}
+        </List>
+      </section>
+    </VisibilitySensor>
+  )
 })
 
 export default Numbers
