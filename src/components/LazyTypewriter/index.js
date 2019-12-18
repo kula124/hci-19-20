430063b10a3a7e1a72479b97da927cbe46c6e54a
Ivan <kula124@gmail.com>
2019-12-18 20:06:52 +0100

    Fira font added (#15)
    * adding fira fonts and appling
    
    * various cosmetic fixes
    
    * typewroter cosmetics (fira)

--- src/components/LazyTypewriter/index.js
+++ src/components/LazyTypewriter/index.js
@@ -5,33 +5,34 @@ import PropTypes from 'prop-types'
 
 import { GenerateRandomString } from 'helper'
 
-const LazyTypewritter = ({ string, tries, ...rest }) => {
+const LazyTypewriter = ({ string, tries, ...rest }) => {
   const substringLen = parseInt(string.length / tries)
   const subs = chunk(string, substringLen).map(e => e.join(''))
   const rnds = chunk(GenerateRandomString(string.length), parseInt(substringLen / 2)).map(e => e.join(''))
 
   let i
 
-  return <Typewriter onInit={(typewriter) => {
-    typewriter.typeString(subs[0])
+  return <Typewriter
+    onInit={(typewriter) => {
+      typewriter.typeString(subs[0])
 
-    for (i = 0; i < tries; i++) {
-      typewriter
-        .typeString(rnds[i])
-        .pauseFor(100)
-        .deleteChars(parseInt(substringLen / 2))
-        .pauseFor(100)
-        .typeString(subs[i + 1])
-    }
-    typewriter.start()
-  }}
-  options={rest}
+      for (i = 0; i < tries; i++) {
+        typewriter
+          .typeString(rnds[i])
+          .pauseFor(100)
+          .deleteChars(parseInt(substringLen / 2))
+          .pauseFor(100)
+          .typeString(subs[i + 1])
+      }
+      typewriter.start()
+    }}
+    options={{ wrapperClassName: 'typewriter', ...rest }}
   />
 }
 
-LazyTypewritter.propTypes = {
+LazyTypewriter.propTypes = {
   string: PropTypes.string,
   tries: PropTypes.number
 }
 
-export default LazyTypewritter
+export default LazyTypewriter
