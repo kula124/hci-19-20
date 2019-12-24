9c69f640a193e11dbd2299785c9f108ce7b4d290
kula <kula124@gmail.com>
2019-12-24 14:35:28 +0100

    adding spacer to page

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,7 @@ import { StoreProvider } from 'store'
 import CrossPlatformSection from 'modules/CrossPlatform'
 import { links } from 'modules/Navigation/navigationData'
 import VisibilitySensor from 'hooks/useIsVisible'
+import AnimatedSpacer from 'components/AnimatedSpacer'
 
 const IndexPage = () => {
   const refs = {
@@ -27,6 +28,7 @@ const IndexPage = () => {
       <VisibilitySensor id={'tech'}>
         <div ref={refs.tech}>
           <NumberSection />
+          <AnimatedSpacer />
           <CrossPlatformSection />
         </div>
       </VisibilitySensor>
