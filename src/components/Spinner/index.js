bc387f967f05ea8a518147fe3ff8f06d7e34ea52
kula <kula124@gmail.com>
2020-01-07 00:05:57 +0100

    Spinenr added

---
+++ src/components/Spinner/index.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+import './style.module.scss'
+
+const Spinner = () =>
+  <div styleName="lds-ellipsis">
+    <div>
+    </div>
+    <div>
+    </div>
+    <div>
+    </div>
+    <div>
+    </div>
+  </div>
+
+export default Spinner
