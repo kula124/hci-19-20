9b8ae586d6f9266f346631e23406c3c9f9b95bfe
kula <kula124@gmail.com>
2020-01-13 16:51:39 +0100

    expanding Revealer to use ...rest

--- src/hooks/useRevealed.js
+++ src/hooks/useRevealed.js
@@ -2,10 +2,11 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import VisibilitySensor from 'react-visibility-sensor'
 
-const Revealer = ({ children }) => {
+const Revealer = ({ children, ...rest }) => {
   const [visible, setVisible] = useState(false)
 
-  return <VisibilitySensor onChange={isVisible => isVisible ? setVisible(true) : null}>
+  return <VisibilitySensor {...rest}
+    onChange={isVisible => isVisible ? setVisible(true) : null}>
     {visible ? children : <div/>}
   </VisibilitySensor>
 }
