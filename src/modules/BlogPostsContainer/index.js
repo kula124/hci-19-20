09b8547761dad24318e5be91f477c419b0017bbb
kula <kula124@gmail.com>
2020-01-13 16:52:40 +0100

    adding blog container

---
+++ src/modules/BlogPostsContainer/index.js
@@ -0,0 +1,70 @@
+import React from 'react'
+import { graphql, useStaticQuery } from 'gatsby'
+
+import { responseMapper } from 'helper'
+import AnimatedSpacer from 'components/AnimatedSpacer'
+import BlogPost from 'layouts/BlogSummary'
+import VisibilitySensor from 'hooks/useIsVisible'
+
+import './style.module.scss'
+
+const mapList = {
+  'posts': 'allContentfulBlogPost.nodes'
+}
+
+const mapBlog = {
+  'body': 'body.body',
+  'coverImage': 'coverImage.fluid',
+  'id': 'body.id',
+  'summary': 'summary.internal.content',
+  'title': 'title',
+  'updatedAt': 'updatedAt'
+}
+
+const listMapper = responseMapper(mapList)
+const blogPostMapper = responseMapper(mapBlog)
+
+// eslint-disable-next-line react/display-name
+const BlogPostContainer = React.forwardRef((props, ref) => {
+  const rawData = useStaticQuery(graphql`query {
+    allContentfulBlogPost {
+      nodes {
+        summary {
+          internal {
+            content
+          }
+        }
+        body {
+          body
+          id
+        }
+        title
+        updatedAt
+        coverImage {
+          fluid {
+            ...GatsbyContentfulFluid_tracedSVG
+          }
+        }
+      }
+    }
+  }`)
+
+  const data = listMapper(rawData).posts.map(blogPostMapper)
+
+  return (
+    <VisibilitySensor id='blog'>
+      <section ref={ref}
+        styleName='main-container'>
+        <h2>Blog</h2>
+        <AnimatedSpacer offset={{ bottom: 100 }}
+          styleProp={'blog'}/>
+        <ul>
+          {data.map(({ id, ...rest }) => <BlogPost key={id}
+            {...rest} />)}
+        </ul>
+      </section>
+    </VisibilitySensor>
+  )
+})
+
+export default BlogPostContainer
