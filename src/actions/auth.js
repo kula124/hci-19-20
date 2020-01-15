2af95541082c05f0185af239b49a431539b94975
kula <kula124@gmail.com>
2020-01-15 14:12:11 +0100

    middleware implementation and localstorage

--- src/actions/auth.js
+++ src/actions/auth.js
@@ -2,6 +2,7 @@
 import { AUTH } from 'constants/actions'
 import { delay } from 'helper'
 import { errors } from 'constants/errors'
+import ls from 'local-storage'
 
 const notAnActualDatabase = [
   {
@@ -21,13 +22,13 @@ const notAnActualDatabase = [
   }
 ]
 
-const inProgress = {
+const inProgress = () => ({
   type: AUTH.IN_PROGRESS,
   data: {}
-}
+})
 
 const loginSuccess = data => {
-  localStorage.setItem('auth', data)
+  ls.set('auth', JSON.stringify(data))
 
   return {
     type: AUTH.SUCCESS,
@@ -42,21 +43,23 @@ const logoutSuccess = data => {
   }
 }
 
-export const loginFailed = message => ({
+const loginFailed = message => ({
   type: AUTH.FAILED,
   error: message
 })
 
-export const logout = async (dispatch) => {
-  localStorage.removeItem('auth')
-  dispatch(logoutSuccess())
-}
+/// ///////////////////////////
 
-export const login = async (dispatch, data) => {
+export const loginFail = message => dispatch =>
+  dispatch(loginFailed(message))
+
+export const logout = () => dispatch => ls.remove('auth') || dispatch(logoutSuccess())
+
+export const login = data => async dispatch => {
   // do fake login kek
   // dispatch in progress first
 
-  dispatch(inProgress)
+  dispatch(inProgress())
   const res = await delay(4000)
     .then(() =>
       notAnActualDatabase.find(el => el.username === data.username && el.password === data.password))
--- src/actions/navigation.js
+++ src/actions/navigation.js
@@ -1,13 +1,13 @@
 import { NAVIGATION } from 'constants/actions'
 
-export const addVisible = data => (
+export const addVisible = data => () => (
   {
     data,
     type: NAVIGATION.ADD_VISIBLE
   }
 )
 
-export const removeVisible = data => (
+export const removeVisible = data => () => (
   {
     data,
     type: NAVIGATION.REMOVE_VISIBLE
