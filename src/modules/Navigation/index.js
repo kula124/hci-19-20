fa78a70fe0c3be2575c16199a77f09ccd1ef77c8
kula <kula124@gmail.com>
2020-02-16 13:51:23 +0100

    preparing for hamburger

--- src/modules/Navigation/index.js
+++ src/modules/Navigation/index.js
@@ -1,19 +1,23 @@
 import React, { useState } from 'react'
+import PropTypes from 'prop-types'
+// import { useMediaQuery } from 'react-responsive'
 
+import Hamburger from 'components/HamburgerNav'
 import { ScrollToRef } from 'helper'
 import Logo from 'components/Logo'
-import PropTypes from 'prop-types'
 import { useStore } from 'store'
 import NavigationItem from './NavigationItem'
-import './style.module.scss'
 import AuthProtector from 'components/AuthProtector'
 import { logout } from 'actions/auth'
+
+import './style.module.scss'
 
 const isElementVisible = (element, nav) => {
   return nav[1] ? nav[1] === element : nav[0] === element
 }
 
 const Navigation = ({ refs, links }) => {
+  const isMobile = false
   const [sticky, setSticky] = useState(false)
   const [visible, setVisible] = useState(false)
   const { store: { navigation, auth }, dispatch } = useStore()
@@ -31,6 +35,13 @@ const Navigation = ({ refs, links }) => {
       }
     }
   }
+
+  const items = links.map(({ id, label, ...rest }) => <NavigationItem key={id}
+    onClick={() => ScrollToRef(refs[id])}
+    styleName={isElementVisible(id, navigation) ? 'selected' : ''}
+    {...rest}>
+    {label}
+  </NavigationItem>)
 
   return (
     <div className={sticky ? 'animated slideInDown' : 'animated fadeInUp'}
@@ -52,14 +63,13 @@ const Navigation = ({ refs, links }) => {
           </NavigationItem>
         </AuthProtector>
       </div>
-      <nav>
-        {links.map(({ id, label, ...rest }) => <NavigationItem key={id}
-          onClick={() => ScrollToRef(refs[id])}
-          styleName={isElementVisible(id, navigation) ? 'selected' : ''}
-          {...rest}>
-          {label}
-        </NavigationItem>)}
-      </nav>
+      {isMobile
+        ? <Hamburger >
+          {items}
+        </Hamburger>
+        : <nav>
+          {items}
+        </nav>}
     </div>
   )
 }
